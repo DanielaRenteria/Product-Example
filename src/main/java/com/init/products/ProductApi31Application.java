22a82b2392f9c2cb8325593f54e75f0535066168
daniela renteria <dani.rnteria21@gmail.com>
2020-11-18 14:21:46 -0600

    mi primer commit

---
+++ src/main/java/com/init/products/ProductApi31Application.java
@@ -0,0 +1,13 @@
+package com.init.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class ProductApi31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApi31Application.class, args);
+	}
+
+}
